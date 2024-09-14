28798de8fbc4d00f6754ee84b3d77309e3320013
Sanetuk <134677494+ljh211118@users.noreply.github.com>
2024-09-14 23:11:15 +0900

    Feat: 그냥 혼자해봄

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -18,7 +18,7 @@ function goToHome() {
                     <div class="app-navbar-item ms-1 ms-md-4">
                         <div class="btn btn-icon btn-custom btn-icon-muted btn-active-light btn-active-color-primary w-35px h-35px"
                             id="kt_activities_toggle" @click="goToHome()">
-                            home
+                            fivegirls
                         </div>
                     </div>
                 </div>
