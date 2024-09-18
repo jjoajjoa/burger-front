dc3ef3ec6a6dc743dc0c88facf5ab2d823040342
Arum <rlfehd429@gmail.com>
2024-09-19 03:33:59 +0900

    Feat: 버거게임 랭킹 기능 구현 아직 하는 중

--- src/views/BurgerKingView.vue
+++ src/views/BurgerKingView.vue
@@ -1,41 +1,93 @@
 <script setup>
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
 import MainHeader from '@/components/MainHeader.vue';
-import MainFooter from '@/components/MainFooter.vue'
+import MainFooter from '@/components/MainFooter.vue';
+
+const burgerTypes = ref([
+    { id: 1, name: 'BURGUNDY' },
+    { id: 2, name: '김숙자 버거' },
+    { id: 3, name: '존버.거' },
+    { id: 4, name: '먹다남은버거' },
+    { id: 5, name: '치즈없으면난못해' }
+]);
+
+const burgerRankings = ref({});
+
+const fetchRankings = async () => {
+    try {
+
+        // const [topResponse, bottomResponse] = await Promise.all([
+        //     axios.get('/api/burgergameking'),
+        //     axios.get('/api/burgergamepoor')
+        // ]);
+
+        const topResponse = await axios.get('/api/burgergameking');
+        const bottomResponse = await axios.get('/api/burgergamepoor');
+
+        console.log('topResponse >>>>>', topResponse);
+        console.log('>>>>>', topResponse.data[0].burgerPk);
+        console.log('bottomResponse >>>>>', bottomResponse);
+
+        /////////////////////////////////////////////////////////////////////////////////
+
+        const rankings = {};
+
+        for (const burger of burgerTypes.value) {
+            rankings[burger.id] = {
+                top: topResponse.data,
+                bottom: bottomResponse.data
+            };
+        }
+
+        burgerRankings.value = rankings;
+    } catch (error) {
+        console.error('BurgerKingView Err >>>>>', error);
+    }
+};
+
+// 컴포넌트 마운트 시 데이터 가져오기
+onMounted(() => {
+    fetchRankings();
+});
 </script>
 
 <template>
     <MainHeader />
 
-    <div class="app-main flex-column flex-row-fluid" id="kt_app_main" style="flex-grow: 1; overflow: auto;">
-        <div class="image-container">
+    <div class="app-main flex-column flex-row-fluid" id="kt_app_main">
+        <div class="image-container position-relative">
             <img src="../assets/images/menu-header.jpg" alt="Menu Header" class="menu-image">
             <h1 class="header-text">Burger King</h1>
         </div>
-        <div class="d-flex flex-column flex-column-fluid" style="flex-grow: 1; justify-content: center; align-items: center;">
-            <div id="kt_app_content" class="app-content flex-column-fluid" style="flex-grow: 1; display: flex; flex-direction: column;">
-                <div id="kt_app_content_container" class="app-container container-xxl tomato" style="flex-grow: 1; padding: 50px 0; display: flex; justify-content: center; align-items: center;">
-                    <div class="olive">
-                        <h1>BURGUNDY</h1>
-                    </div>
-                    <div>
-                        <h1>김숙자 버거</h1>
-                    </div>
-                    <div>
-                        <h1>존버.거</h1>
-                    </div>
-                    <div>
-                        <h1>먹다남은버거</h1>
-                    </div>
-                    <div>
-                        <h1>치즈없으면난못해</h1>
-                    </div>
 
+        <div class="d-flex flex-column flex-column-fluid justify-content-center align-items-center olive">
+            <div id="kt_app_content_container"
+                class="app-container container-xxl d-flex my-10 flex-wrap justify-content-between align-items-center">
+                <div v-for="burger in burgerTypes" :key="burger.id" class="card-container-burger">
+                    <h1>{{ burger.name }}</h1>
+                    <div class="card-burger" v-if="burgerRankings[burger.id] && burgerRankings[burger.id].top">
+                        <h2>버거킹</h2>
+                        <p v-for="(item, index) in burgerRankings[burger.id].top" :key="'top-' + index">{{ index + 1 }}등
+                            : {{ item.user_name }}</p>
+                    </div>
+                    <div class="card-burger" v-else>
+                        <h2>버거킹</h2>
+                        <p>데이터가 없습니다</p>
+                    </div>
+                    <div class="card-burger" v-if="burgerRankings[burger.id] && burgerRankings[burger.id].bottom">
+                        <h2>버거지</h2>
+                        <p v-for="(item, index) in burgerRankings[burger.id].bottom" :key="'bottom-' + index">{{ index +
+                            1 }}등 : {{ item.user_name }}</p>
+                    </div>
+                    <div class="card-burger" v-else>
+                        <h2>버거지</h2>
+                        <p>데이터가 없습니다</p>
+                    </div>
                 </div>
             </div>
-
         </div>
     </div>
 
     <MainFooter />
-
 </template>
